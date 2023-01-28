c70fd74437c361fd3b4a6e5a31a942e2783cbd9f
Ashheidari <Heidariarash1@gmail.com>
2023-01-28 10:05:33 -0800

    Sqrtx solved with binary search approach

---
+++ Sqrtx.js
@@ -0,0 +1,21 @@
+const mySqrt = function(x) {
+    let low = 1;
+    let high = x;
+    let ans = 0;
+    while(low<=high){
+        let mid = Math.floor((high+low)/2);
+        if(x/mid === mid){return mid}
+        else if (x/mid < mid){
+            high = mid - 1
+        }
+        else{
+            low = mid + 1
+            ans = mid
+        }
+
+    }
+    return ans
+};
+
+
+console.log(mySqrt(145));
